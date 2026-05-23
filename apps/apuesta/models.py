5b3a40274a0aa7c951ffac6021f7dfa629cea258
exeluna16 <exeluna666@gmail.com>
2026-05-22 21:28:38 -0300

    se creo el modelo partido

--- apps/apuesta/models.py
+++ apps/apuesta/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Partido(models.Model):
+
+    equipo_local = models.CharField()
+    equipo_visitante = models.CharField()
+    goles_local = models.IntegerField()
+    goles_visitante = models.IntegerField()
+    resultado_partido = models.IntegerField()
+    tarjetas_amarillas = models.IntegerField()
+    tarjetas_rojas = models.IntegerField()
+    estado = models.CharField()
+    id_apifootball = models.CharField()
+    fecha = models.DateTimeField() #posiblemente se pueda usar null
