b99ed526c1a4ca6c180997e11aacde636fc319c7
arielgonzalo <aibargonzalo0@gmail.com>
2026-05-23 23:35:44 -0300

    se borro api.py

--- apps/apuesta/urls.py
+++ apps/apuesta/urls.py
@@ -1,6 +1,6 @@
 from rest_framework.routers import DefaultRouter
 
-from .api import PartidoViewSet
+from .views import PartidoViewSet
 
 router = DefaultRouter()
 router.register(r'partidos', PartidoViewSet)
