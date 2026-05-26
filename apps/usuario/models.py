fbab0710a987f3f15222bd6f243538fadacdd099
exeluna16 <exeluna666@gmail.com>
2026-05-26 15:37:58 -0300

    se actualizo el modelo [Usuario]

--- apps/usuario/models.py
+++ apps/usuario/models.py
@@ -1,15 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from numpy.ma.core import negative
 
 
 # Create your models here.
 
 class Usuario(AbstractUser):
     nombre = models.CharField(max_length=50,null=False,blank=False)
+    apellido = models.CharField(max_length=50,null=False,blank=False)
     cuil = models.CharField(max_length=11,verbose_name="cuil",unique=True)
     numero_de_cuenta = models.CharField(max_length=30,null=False,blank=False)
-    saldo = models.DecimalField(default=0,null=False,blank=False)
+    saldo = models.DecimalField(default=0,null=False,blank=False,decimal_places=2,max_digits=20)
 
     def __str__(self):
         return f'Usuario:  {self.nombre}'
