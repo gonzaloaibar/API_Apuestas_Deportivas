b4627f73665af8e41976db6880905613bce5f582
exeluna16 <exeluna666@gmail.com>
2026-05-31 20:06:18 -0300

    se agrego la documentacion

--- API_Apuestas_Deportivas/settings.py
+++ API_Apuestas_Deportivas/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework_simplejwt',
+    'drf_spectacular',
     'apps.apuesta',
     'apps.servicio',
     'apps.usuario',
@@ -146,6 +147,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.DjangoModelPermissions',
         'rest_framework.permissions.IsAuthenticated',
     ],
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
     # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     # 'PAGE_SIZE': 10,
 }
