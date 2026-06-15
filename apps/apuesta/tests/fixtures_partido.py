68be157b7b02518ee3473946fb4c57186873669e
arielgonzalo <aibargonzalo0@gmail.com>
2026-06-15 17:38:00 -0300

    modificado el fixture partido

--- apps/apuesta/tests/fixtures_partido.py
+++ apps/apuesta/tests/fixtures_partido.py
@@ -5,6 +5,7 @@ from apps.apuesta.models import Partido
 def get_partido():
 
     return Partido.objects.create(
+        api_football_id = "123738",
         equipo_local="River",
         equipo_visitante="Boca",
         estado="pendiente"
