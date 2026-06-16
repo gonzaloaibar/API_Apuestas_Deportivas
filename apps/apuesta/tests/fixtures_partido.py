f618f09cdd5bb7e21b007e98047b7b2b52139ff1
exeluna16 <exeluna666@gmail.com>
2026-06-16 01:39:00 -0300

    se crearon test para opcion de apuesta

--- apps/apuesta/tests/fixtures_partido.py
+++ apps/apuesta/tests/fixtures_partido.py
@@ -2,6 +2,25 @@ import pytest
 from apps.apuesta.models import Partido
 
 @pytest.fixture
+def crear_partidos():
+    def _crear(api_football_id,
+       equipo_local,
+       equipo_visitante,
+       estado,
+       fecha
+    ):
+        return Partido.objects.create(
+            api_football_id = api_football_id,
+            equipo_local = equipo_local,
+            equipo_visitante = equipo_visitante,
+            estado = estado,
+            fecha = fecha,
+        )
+
+    #retornar la funcion me va a permitir crear varios partidos
+    return _crear
+
+@pytest.fixture
 def get_partido():
 
     return Partido.objects.create(
@@ -9,4 +28,14 @@ def get_partido():
         equipo_local="River",
         equipo_visitante="Boca",
         estado="pendiente"
+    )
+
+@pytest.fixture
+def get_partido_finalizado():
+
+    return Partido.objects.create(
+        api_football_id = "123738",
+        equipo_local="River",
+        equipo_visitante="Boca",
+        estado="finalizado"
     )
